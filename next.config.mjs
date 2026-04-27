52a643393cafb990f8b081a22cdff4297c5911ce
Frank <frank@MacBook-Air-de-Frank.local>
2026-04-27 12:22:24 -0300

    fix: turbopack build issue

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  // força desativar conflitos com turbopack
+  turbopack: {},
 };
 
 export default nextConfig;
