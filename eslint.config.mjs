ee6df75e0314bf5a124fe0a9728bc793b9b63026
Frank <frank@MacBook-Air-de-Frank.local>
2026-04-27 15:29:12 -0300

    migrate eslint to modern config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,12 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { defineConfig, globalIgnores } from "eslint/config";
+import next from "eslint-config-next/core-web-vitals.js";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
-
-export default eslintConfig;
+export default defineConfig([
+  next,
+  globalIgnores([
+    ".next/**",
+    "out/**",
+    "build/**",
+    "next-env.d.ts",
+  ]),
+]);
