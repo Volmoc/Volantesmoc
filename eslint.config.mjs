34654c3d26a3126c9c9e44d8cc856ca29d114aaf
Frank <frank@MacBook-Air-de-Frank.local>
2026-04-27 15:34:13 -0300

    fix eslint config for next build

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,3 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import next from "eslint-config-next/core-web-vitals.js";
+import next from "eslint-config-next";
 
-export default defineConfig([
-  next,
-  globalIgnores([
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-]);
+export default [next];
